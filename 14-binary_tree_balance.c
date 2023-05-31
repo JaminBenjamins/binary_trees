8122670681c6d684d3470ce528d3363f7eb4a0f3
jaminbenjamins <jaminbenjamins@gmail.com>
2023-05-31 21:16:53 +0300

    Check if tree depth is equal

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree);
+/**
+ * binary_tree_balance - a function that calculates balance of a tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_height - a function that calculates height of a tree
+ * @tree: the pointer to root node of the tree
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (l > r ? l : r);
+	}
+	return (0);
+}
+
