b2dec56c0a0cf7e4ea837b2e86f613ef64b6f5d0
jaminbenjamins <jaminbenjamins@gmail.com>
2023-06-02 02:06:32 +0300

    Adding function arguments

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,12 +7,12 @@
  *
  * Return: nothing
  */
-void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_inorder(tree->left);
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_inorder(tree->right);
+		binary_tree_inorder(tree->right, func);
 	}
 }
