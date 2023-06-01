e89d9a81b041a4149fe267435e5db8423078fcee
jaminbenjamins <jaminbenjamins@gmail.com>
2023-06-02 02:29:36 +0300

    Correct return option of ternary operator

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - a function that calculates balance of a tree
  * @tree: pointer to the root node of the tree
@@ -28,10 +27,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return (l > r ? l : r);
 	}
 	return (0);
 }
-
